a3eea5b22e441f1f39ef502106f7f8d3aa4da9b0
Vis <Vish106@rambler.ru>
2022-08-11 20:22:55 +0300

    Итерация 2

---
+++ Assets/Scripts/HashAnimationNames.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public static class HashAnimationNames 
+{
+    public static class PlayerAnimation
+    {
+        public static int SpeedHash = Animator.StringToHash("Speed");
+    } 
+
+    public static class HouseAnimation
+    {
+        public static int IsClosed = Animator.StringToHash("IsClosed");
+    }
+}
---
+++ Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerZone : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _enteredZone;
+    [SerializeField] private UnityEvent _leftZone;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player))
+        {
+            _enteredZone?.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player))
+        {
+            _leftZone?.Invoke();
+        }
+    }
+}

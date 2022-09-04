b6ff0553ba5d398656204dfcf789c3c691dc5840
Vis <Vish106@rambler.ru>
2022-09-04 08:43:45 +0300

    Добавлен спавнер

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private Vector3 _offset;
+    [SerializeField] private float _smoothFactor;
+
+    private void Update()
+    {
+        Folllow();
+    }
+
+    private void Folllow()
+    {
+        Vector3 targetPosition = _target.position + _offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, _smoothFactor * Time.deltaTime);
+        transform.position = smoothPosition;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : ObjectPool
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private float _secondsBetweenSpawn;
+    [SerializeField] private Transform[] _spawnPoints;
+
+    private float _elepsedTime = 0;
+
+    private void Start()
+    {
+        Initialize(_enemyPrefab);
+    }
+
+    private void Update()
+    {
+        _elepsedTime += Time.deltaTime;
+
+        if (_elepsedTime >= _secondsBetweenSpawn)
+        {
+            if (TryGetObject(out GameObject enemy))
+            {
+                _elepsedTime = 0;
+                int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
+
+                SetEnemy(enemy, _spawnPoints[spawnPointNumber].position);
+            }
+        }
+    }
+
+    private void SetEnemy(GameObject enemy, Vector3 spawnPoint)
+    {
+        enemy.SetActive(true);
+        enemy.transform.position = spawnPoint;
+    }
+}
+
